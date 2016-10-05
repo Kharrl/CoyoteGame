a2e3cb27b1fc5443216d7a604359f3fcda67bef1
Taylor Tomlinson <Kharrlart@gmail.com>
2016-10-05 15:12:17 -0600

    Working on Character flip, Modeling scene assets.

---
+++ Star Catcher/Assets/Level/CharacterFlip.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlip : MonoBehaviour {
+
+	public Transform characterArt;
+	public bool forward = true;
+	// Use this for initialization
+	void flipCharacter (KeyCode _keycode)
+	{
+		switch (_keycode) {
+		case KeyCode.RightArrow:
+			characterArt.Rotate(0,180,0);
+			break;
+
+		case KeyCode.LeftArrow:
+			characterArt.Rotate(0,180,0);
+			break;
+		}	
+			
+	}
+	void Start()
+	{
+		
+		UserInput.UserInputs += flipCharacter;
+	}
+}
---
+++ Star Catcher/Assets/Level/UserInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+
+public class UserInput : MonoBehaviour {
+	public static Action<KeyCode> UserInputs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow) && UserInputs != null) 
+		{
+			UserInputs (KeyCode.RightArrow);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && UserInputs != null)
+		{
+			UserInputs (KeyCode.LeftArrow);
+		}	
+	 
+	}
+}
