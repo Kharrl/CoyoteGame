a90fb9ba55256afd0c7281e2ba01c3501ec6af74
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-04 22:47:56 -0600

    created StaticVar script containing, health, ammo, score, and gametime

---
+++ NotFunGameForAnthony/Assets/StaticVars.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticVars : MonoBehaviour {
+	public int playerHealth = 10;
+	public float GameTime = 100f;
+	public int Ammo = 0;
+	public int playerScore = 0;
+
+}
