84ffaf5343bad11ec0324fe4fb37d47216e4b830
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-05 20:14:42 -0700

    Started adding a userinput and player move script

---
+++ NotFunGameForAnthony/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+	public float playerspeed =10f;
+	private CharacterController myController;
+	private Vector3 tempPos;
+	private Transform newPos;
+	// Use this for initialization
+	void Start () {
+		myController = GetComponent<CharacterController>();
+		userupdate.userinputs += userinputhandler;
+	}
+	void userinputhandler(userupdate _KeyCode)
+	{
+		switch (Input) {
+		case KeyCode.A:
+			tempPos.x = -playerspeed;
+			myController.Move (tempPos);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- NotFunGameForAnthony/Assets/Scripts/userupdate.cs
+++ NotFunGameForAnthony/Assets/Scripts/userupdate.cs
@@ -12,6 +12,5 @@ public class userupdate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Mathf.Round(StaticVars.GameTime -= Time.deltaTime);
 	}
 }
