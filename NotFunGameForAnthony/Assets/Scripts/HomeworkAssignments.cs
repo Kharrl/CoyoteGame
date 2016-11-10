4142d9b2a9798d75c087f64aa9a20a5d105f8bfb
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-10 15:10:20 -0700

    Homework Assignment: Variables completed.

--- NotFunGameForAnthony/Assets/Scripts/HomeworkAssignments.cs
+++ NotFunGameForAnthony/Assets/Scripts/HomeworkAssignments.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class HomeworkAssignments : MonoBehaviour {
+	public int EnemyWave = 10;
+	/* Variables are containers that hold various types of data. They can be values
+	 * such as int for whole numbers and floats for decimals, they can be game objects,
+	 * components, or bools, which are true/false statements. In order to use a variable
+	 * in your script you must first declare it like i have above. 
+	 */
 
 	// Use this for initialization
 	void Start () {
