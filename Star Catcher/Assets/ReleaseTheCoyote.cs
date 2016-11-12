0670fe6bdca2f017356a269781870d9b1fe0b387
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-12 13:45:19 -0700

    Randomized the Coytoe's speed on spawn.

--- Star Catcher/Assets/ReleaseTheCoyote.cs
+++ Star Catcher/Assets/ReleaseTheCoyote.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class ReleaseTheCoyote : MonoBehaviour {
 	private NavMeshAgent myAgent;
+	public float CoyoteSpeed;
 	public float SpawnTime;
 	// Use this for initialization
 	void Start () {
 		myAgent = GetComponent<NavMeshAgent> ();
 		SpawnTime = Random.Range (5f, 10f);
+		CoyoteSpeed = Random.Range (15f, 45f);
 		myAgent.enabled = false;
 		StartCoroutine (CoyoteActive ());
 	}
@@ -16,6 +18,7 @@ public class ReleaseTheCoyote : MonoBehaviour {
 	IEnumerator CoyoteActive()
 	{
 		yield return new WaitForSeconds (SpawnTime);
+		myAgent.speed = CoyoteSpeed;
 		myAgent.enabled = true;
 	}
 }
