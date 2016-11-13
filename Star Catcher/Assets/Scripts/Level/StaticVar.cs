f6fc0b5dde89e5136c7e75c3f3b7bdeed91a4da5
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-12 23:30:41 -0700

    Added and resized the burrow at the end. Changed level time to 120 seconds

--- Star Catcher/Assets/Scripts/Level/StaticVar.cs
+++ Star Catcher/Assets/Scripts/Level/StaticVar.cs
@@ -8,7 +8,7 @@ public class StaticVar{
 	public static float JumperCount=0;
 	public static bool Grounded=true;
 	public static int StarsCollected=0;
-	public static float GameClock =100f;
+	public static float GameClock =120f;
 	public static float MaxStarPower=100f;
 
 
