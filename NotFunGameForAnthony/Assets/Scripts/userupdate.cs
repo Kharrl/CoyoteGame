1bf91a9cd8f8fac4c914d1bd8ef7f5e4cdfd1af3
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-14 19:36:06 -0700

    NavMesh working, character can move, and zombie dies when shot if it is in range

--- NotFunGameForAnthony/Assets/Scripts/userupdate.cs
+++ NotFunGameForAnthony/Assets/Scripts/userupdate.cs
@@ -16,23 +16,7 @@ public class userupdate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-		
-		if (Input.GetKeyDown (KeyCode.RightArrow) && UserInputs!=null) 
-		{
-			UserInputs (KeyCode.RightArrow);
-		}
-		if (Input.GetKeyDown (KeyCode.LeftArrow) && UserInputs != null)
-		{
-			UserInputs (KeyCode.LeftArrow);
-		}	
-		if (Input.GetKeyDown (KeyCode.UpArrow) && UserInputs != null) {
-			UserInputs (KeyCode.UpArrow);
-		}
-		if (Input.GetKeyDown (KeyCode.DownArrow) && UserInputs != null) {
-			UserInputs (KeyCode.DownArrow);
-		}
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.RightCommand)) {
 			print ("Gun Fired");
 			FireGun (this);
 		}
