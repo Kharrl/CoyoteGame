9a6649383fc2c4e3a72e601090f77f1c0fadafc5
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-27 01:30:02 -0700

    Added a script that continued the canvas through the changing of cameras.

---
+++ Star Catcher/Assets/CanvasControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasControl : MonoBehaviour {
+	public Canvas myCanvas;
+	public Camera blimpCamera;
+	public Camera finalCam;
+	// Use this for initialization
+	void Start () {
+		KillBunny.Kill += KillHandler;
+		ExitBlimp.Landed += LandedHandler;
+	}
+	
+	// Update is called once per frame
+	void KillHandler (KillBunny obj) {
+		myCanvas.worldCamera = blimpCamera;
+	}
+	void LandedHandler(Transform obj)
+	{
+		myCanvas.worldCamera = finalCam;
+	}
+}
