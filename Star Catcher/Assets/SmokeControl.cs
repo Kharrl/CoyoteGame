7b0684b96dc455779903adef06874936314ed4b1
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-27 02:19:45 -0700

    Added Visual affects, made small tweaks on layout.

---
+++ Star Catcher/Assets/SmokeControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmokeControl : MonoBehaviour {
+	public GameObject Smoke1;
+	public GameObject Smoke2;
+	// Use this for initialization
+	void Start () {
+		Smoke1.SetActive (false);
+		Smoke2.SetActive (false);
+		KillBunny.Kill += GetInHandler;
+	}
+	
+	// Update is called once per frame
+	void GetInHandler (KillBunny obj) {
+		Smoke1.SetActive (true);
+		Smoke2.SetActive (true);
+	}
+}
---
+++ Star Catcher/Assets/startSparks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startSparks : MonoBehaviour {
+	public GameObject Sparky;
+	// Use this for initialization
+	void Start () {
+		Sparky.SetActive (false);
+		RidetheLift.Triggered += TriggeredHandler;
+	}
+	
+	// Update is called once per frame
+	void TriggeredHandler (RidetheLift obj) {
+		Sparky.SetActive (true);
+	}
+}
