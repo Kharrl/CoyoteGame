012e9a1adcf6508bc3b81681c7d77d02ffe74f10
Taylor Tomlinson <Kharrlart@gmail.com>
2016-11-29 22:43:11 -0700

    rescripting the coyote.

---
+++ Star Catcher/Assets/ActivateCoyote.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateCoyote : MonoBehaviour {
+	public Transform player;
+	public NavMeshAgent coyote;
+
+	void Start(){
+		coyote = GetComponent<NavMeshAgent> ();
+		coyote.enabled = false;
+		ExitBlimp.Landed = LandedHandler;
+	}
+	void LandedHandler(Transform newPlayer)
+	{
+		player = newPlayer;
+		coyote.destination = player.position;
+	}
+	void OnWillRenderObject()
+	{
+		coyote.enabled = true;
+	}
+
+
+}
