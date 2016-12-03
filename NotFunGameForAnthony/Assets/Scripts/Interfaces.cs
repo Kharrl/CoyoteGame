c92b91a6966c283e4829a957372ad2d4afcde1a7
Taylor Tomlinson <Kharrlart@gmail.com>
2016-12-02 21:02:04 -0700

    Made Some Interfaces and a weapon script to inherit from

---
+++ NotFunGameForAnthony/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IWeapon{ 
+
+	int WeaponSpeed();
+	int WeaponDmg();
+
+}
+public interface IEnemy{
+	int Health();
+	int StalkSpeed ();
+	void Attack ();
+}
---
+++ NotFunGameForAnthony/Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WeaponBase : MonoBehaviour {
+	public static Action<float> SendDmg;
+	public float WeaponDmg;
+	public float WeaponSpeed;
+	public bool IsRangedWeapon;
+	public Transform Player;
+
+
+	// Use this for initialization
+	void Start () {
+		userupdate.UserInputs += UserInputHandler;
+	}
+	
+	// Update is called once per frame
+	void UserInputHandler (KeyCode _K) {
+		switch(_K)
+		{
+		case KeyCode.Z:
+			if (IsRangedWeapon == true)
+				StartCoroutine (WeaponDelay ());
+			break;
+		case KeyCode.X:
+			if (IsRangedWeapon == false)
+				StartCoroutine (MeleeAttack ());
+			break;
+		default:
+			break;
+	}
+}
+	IEnumerator WeaponDelay()
+	{
+		SendDmg (WeaponDmg);
+		yield return new WaitForSeconds (WeaponSpeed);
+	}
+	IEnumerator MeleeAttack()
+	{
+		this.transform.RotateAround (Player.position, Vector3.up, 90*Time.deltaTime);
+		yield return new WaitForSeconds (WeaponSpeed);
+		this.transform.RotateAround (Player.position, Vector3.up, -90 * Time.deltaTime);
+		yield return new WaitForSeconds (WeaponSpeed);
+		StopCoroutine (MeleeAttack ());
+	}
+}
